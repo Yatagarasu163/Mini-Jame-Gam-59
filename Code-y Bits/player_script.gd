a8e0e6777231b28e9cbd61fe4e07539e006a5646
BarrelPanda <samuel.leong.2006@gmail.com>
2026-09-18 23:02:08 +0800

    spawner donez

--- Code-y Bits/player_script.gd
+++ Code-y Bits/player_script.gd
@@ -1,11 +1,11 @@
 extends CharacterBody2D
 
-@export var _speed: float
+@export var _speed: float = 500.0
 
-var _tomato_fresh: int
-var _tomato_cut: int
-var _tomato_cooked: int
-var _tomato_bottled: int
+var _tomato_fresh: int = 0
+var _tomato_cut: int = 0
+var _tomato_cooked: int = 0
+var _tomato_bottled: int = 0
 
 func _physics_process(_delta: float) -> void:
 	var direction := Input.get_vector("move_left", "move_right", "move_up", "move_down")
