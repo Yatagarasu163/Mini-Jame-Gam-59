68f11b9ad1fc8fccf6e694485ad86327aaf078c8
ZEPHYRRR-pixel <saifmusthafa05@gmail.com>
2026-09-19 22:54:44 +0800

    Factory Machine Logic flow done
    Just need to create the mini game for Cooking, blending and bottling

--- Code-y Bits/factory_machine.gd
+++ Code-y Bits/factory_machine.gd
@@ -1,6 +1,9 @@
 extends Node
 
 var machineActive = false
+var playerInRange = false
+var playerNearOutput = false
+var player = null
 var currentPress = 0
 var requiredPress = 0
 
@@ -23,14 +26,49 @@ enum MachineType { CUTTER,COOKING,BLENDER,BOTTLING}
 @export var keyIndicator: Panel
 @export var keyLabel: Label
 @export var instructionText: Label
+@export var outputItem: Area2D
 
 func _ready():
 	cuttingProgressBar.visible = false
 	keyIndicator.visible = false
 	instructionText.visible = false
+	
 func interact():
 	if machineActive == false:
-		startMiniGame()
+		
+		if machineType == MachineType.CUTTER:
+			if player.enough_fresh_tomatoes():
+				player.get_node("CarryPoint/FreshTomato").visible = false
+				startMiniGame()
+			else:
+				print("You need 5 tomatoes!")
+		
+		if machineType == MachineType.COOKING:
+			if player.enough_cut_tomatoes():
+				var carriedItem = player.get_node("CarryPoint/OutputItem")
+				carriedItem.reparent(get_parent().get_node("CutterMachine/OutputPoint"))
+				carriedItem.visible = false
+				startMiniGame()
+			else:
+				print("You need a Cut Tomato!")
+		
+		if machineType == MachineType.BLENDER:
+			if player.enough_cooked_tomatoes():
+				var carriedItem = player.get_node("CarryPoint/OutputItem")
+				carriedItem.reparent(get_parent().get_node("CookingMachine/OutputPoint"))
+				carriedItem.visible = false
+				startMiniGame()
+			else:
+				print("You need a Cooked Tomato!")
+		
+		if machineType == MachineType.BOTTLING:
+			if player.enough_blended_tomatoes():
+				var carriedItem = player.get_node("CarryPoint/OutputItem")
+				carriedItem.reparent(get_parent().get_node("BlenderMachine/OutputPoint"))
+				carriedItem.visible = false
+				startMiniGame()
+			else:
+				print("You need a Blended Tomato!")
 			
 func startMiniGame():
 	machineActive = true
@@ -65,19 +103,26 @@ func startCuttingGame():
 	print("Selected Key: ", selectedKeys)
 	
 func startCookingGame():
-	pass
+	completeMiniGame()
 	
 func startBlenderGame():
-	pass
+	completeMiniGame()
 	
 func startBottlingGame():
-	pass
+	completeMiniGame()
 
 func chooseRandomKey():
 	selectedKeys = possibleKeys.pick_random()
 	
 func _process(delta: float) -> void:
+	if playerNearOutput and outputItem.visible:
+		if Input.is_action_just_pressed("interact"):
+			pickupOutput()
+	
 	if machineActive == false:
+		return
+	
+	if playerInRange == false:
 		return
 	
 	match machineType:
@@ -92,7 +137,27 @@ func _process(delta: float) -> void:
 		
 		MachineType.BOTTLING:
 			updateBottlingGame(delta)
+
+func pickupOutput():
+	if machineType == MachineType.CUTTER:
+		player.add_cut_tomato()
 		
+	if machineType == MachineType.COOKING:
+		player.add_cooked_tomato()
+		
+	if machineType == MachineType.BLENDER:
+		player.add_blended_tomato()
+		
+	if machineType == MachineType.BOTTLING:
+		player.add_bottled_tomato()
+		
+	outputItem.reparent(player.get_node("CarryPoint"))
+	outputItem.position = Vector2.ZERO
+	outputItem.monitoring = false
+	
+	playerNearOutput = false
+	print("Output Picked Up")
+	
 func updateCuttingGame():
 	if Input.is_action_just_pressed(selectedKeys):
 		currentPress += 1
@@ -116,21 +181,68 @@ func completeMiniGame():
 	machineActive = false
 	
 	if machineType == MachineType.CUTTER:
+		player.finish_cutting()
 		cuttingProgressBar.visible = false
 		keyIndicator.visible = false
 		instructionText.visible = false
+		print("Cutting Complete")
+		
+	if machineType == MachineType.COOKING:
+		player.finish_cooking()
+		print("Cooking Completed")
+		
+	if machineType == MachineType.BLENDER:
+		player.finish_blending()
+		print("Blending Complete")
+		
+	if machineType == MachineType.BOTTLING:
+		player.finish_bottling()
+		print("Bottling Complete")
 	
-	print("Machine Complete!")					
+	outputItem.reparent($OutputPoint)
+	outputItem.position = Vector2.ZERO
+	outputItem.visible = true
+	outputItem.monitoring = true
+	
+	print("Machine Complete!")
+	
+func showMiniGameUI():
+	if  machineType == MachineType.CUTTER:
+		cuttingProgressBar.visible = true
+		keyIndicator.visible = true
+		instructionText.visible = true
 
+func hideMiniGameUI():
+	if machineType == MachineType.CUTTER:
+		cuttingProgressBar.visible = false
+		keyIndicator.visible = false
+		instructionText.visible = false
 
 func _on_interaction_area_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
+		playerInRange = true
+		player = body
 		body.get_node("Interaction").setNearbyMachine(self)
+		
+		if machineActive:
+			showMiniGameUI()
+			
 
 
 func _on_interaction_area_body_exited(body: Node2D) -> void:
 	if body.name == "Player":
+		playerInRange = false
 		body.get_node("Interaction").notNearMachine()
-
-
 		
+		if machineActive:
+			hideMiniGameUI()
+
+
+func _on_output_item_body_entered(body: Node2D) -> void:
+	if body.name == "Player":
+		playerNearOutput = true
+		player = body
+
+func _on_output_item_body_exited(body: Node2D) -> void:
+	if body.name == "Player":
+		playerNearOutput = false
