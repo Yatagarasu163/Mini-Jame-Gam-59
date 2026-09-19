c77eacf94d8875c23e7b921a7cd9f468dbc260e7
Kur3nai <darkserr0@gmail.com>
2026-09-20 04:17:12 +0800

    Separated the Main Menu and the Settings into different scenes
    -added a global script to toggle the setting panel via esc
    -added an audio bus
    -abit buggy interms of music

--- Code-y Bits/brightness_control.gd
+++ Code-y Bits/brightness_control.gd
@@ -1,4 +1,8 @@
 extends HSlider
 
+
+func _ready() -> void:
+	value = GlobalWorldEnvironment.environment.adjustment_brightness
+
 func _on_value_changed(value: float) -> void:
 	GlobalWorldEnvironment.environment.adjustment_brightness = value
--- global.gd
+++ global.gd
@@ -1,11 +1,44 @@
 extends Node
 
+var is_menu_open: bool = false
+var options_menu_instance: Node = null
 
-# Called when the node enters the scene tree for the first time.
+const OPTIONS_MENU_SCENE = preload("res://options_menu.tscn")
+
+
 func _ready() -> void:
-	pass # Replace with function body.
+	# Allow ESC to work while the game is paused
+	process_mode = Node.PROCESS_MODE_ALWAYS
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _unhandled_input(event: InputEvent) -> void:
+	var current_scene = get_tree().current_scene
+	
+	if current_scene == null:
+		return
+	
+	# Only allow the options menu in main.tscn
+	if current_scene.scene_file_path != "res://Scenes/main.tscn":
+		return
+	
+	if event.is_action_pressed("ui_cancel"):
+		toggle_options_menu()
+
+
+func toggle_options_menu() -> void:
+	is_menu_open = !is_menu_open
+	
+	if is_menu_open:
+		options_menu_instance = OPTIONS_MENU_SCENE.instantiate()
+		get_tree().root.add_child(options_menu_instance)
+		
+		options_menu_instance.process_mode = Node.PROCESS_MODE_WHEN_PAUSED
+		options_menu_instance.exit_option_menu.connect(toggle_options_menu)
+		
+		get_tree().paused = true
+		
+	else:
+		if is_instance_valid(options_menu_instance):
+			options_menu_instance.queue_free()
+		
+		get_tree().paused = false
