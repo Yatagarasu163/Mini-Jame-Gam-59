5f594b184ee5b88b72368f22eb3d094a67579385
BarrelPanda <samuel.leong.2006@gmail.com>
2026-09-20 15:59:39 +0800

    fixed room generation

--- Code-y Bits/factory_room.gd
+++ Code-y Bits/factory_room.gd
@@ -1,15 +1,12 @@
 extends Node2D
+class_name factory_room
 
+@export var _machines: Array[Node2D]
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	for machine in _machines:
+		machine.visible = false
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-func generate():
-	print("Room (" + str(position.x / 640) + ", " + str(position.y / 640) + ") Generated")
-	pass
+func generate(type: int):
+	_machines[type].visible = true
