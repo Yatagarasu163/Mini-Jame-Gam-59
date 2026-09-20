6fcae207368d76dc9cc28d29a9749aa8de2c59e1
BarrelPanda <samuel.leong.2006@gmail.com>
2026-09-20 22:27:12 +0800

    fixed tomato spawner position

--- Code-y Bits/factory_spawner.gd
+++ Code-y Bits/factory_spawner.gd
@@ -25,7 +25,7 @@ func SpawnFactory():
 			current_room.position = Vector2(new_x * x_multiplier, new_y * y_multiplier)
 			all_rooms.append(current_room)
 			add_child(current_room)
-	for i in range(4):
+	for i in range(5):
 		var selected_room_index = randi() % len(all_rooms)
 		all_rooms[selected_room_index].generate(i)
 		all_rooms.remove_at(selected_room_index)
